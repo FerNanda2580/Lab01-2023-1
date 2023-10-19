4e889d1dfca74b9f3b8e55218ab01217aa843db8
Nandinha <103125381+FerNanda2580@users.noreply.github.com>
2023-10-19 20:30:05 +0000

    terminar

--- src/main/App.java
+++ src/main/App.java
@@ -1,14 +1,43 @@
 package main;
 public class App {
-    public static int encontre(int[] arrayOrdenado, int num){
-        return -1;
+        public static int encontre(int[] arrayOrdenado, int num){
+
+          for (int i = 0; i<arrayOrdenado.length; i++){
+            if (arrayOrdenado [1] == num ){
+             
+                return 1;
+         }
+           
+            else {
+               return 0;  
+        }
+
+    }
+    return -1;
+
+        
     }
 
     public static int maior(int[] arrayDesordenado){
+        int m = 0; 
+        for (int i = 0; i<arrayDesordenado.length; i++){
+            if (m<arrayDesordenado[i]){
+               (m=arrayDesordenado[i]);
+
+                
+            }
+
+
+        }
+
+
+
         return -1;
+
     }
 
     public static Integer parMaisProximo(Integer[][] pares){
         return 1;
     }
 }
+ 
