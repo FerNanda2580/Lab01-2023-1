54b5fd1cf74d78087b1210081ffda2bc673c3a7a
Nandinha <103125381+FerNanda2580@users.noreply.github.com>
2023-10-20 03:32:37 +0000

    mudado

--- src/main/App.java
+++ src/main/App.java
@@ -2,39 +2,47 @@ package main;
 public class App {
         public static int encontre(int[] arrayOrdenado, int num){
 
-          for (int i = 0; i<arrayOrdenado.length; i++){
-            if (arrayOrdenado [1] == num ){
-             
+          int esq = 0;
+          int dir = arrayOrdenado.length - 1;
+
+          while (esq <= dir){
+            int mei = esq + (dir - esq) /2;
+            if (arrayOrdenado[mei] == num){
                 return 1;
-         }
-           
+            }
+            if (arrayOrdenado[mei]< num){
+                esq = mei + 1;
+            }
             else {
-               return 0;  
+                dir = mei - 1;
+            }
         }
+        return 0;
 
-    }
-    return -1;
-
-        
-    }
+}
 
     public static int maior(int[] arrayDesordenado){
-        int m = 0; 
-        for (int i = 0; i<arrayDesordenado.length; i++){
-            if (m<arrayDesordenado[i]){
-               (m=arrayDesordenado[i]);
-
-                
-            }
-
-
+        
+        if(arrayDesordenado.length == 0){
+            System.out.println("Vazio");
         }
 
+        int maiorNum = arrayDesordenado[0];
+        for (int i = 1; i < arrayDesordenado.length; i++){ 
+    
+           
+            if (arrayDesordenado[i] > maiorNum){
 
-
-        return -1;
-
+                maiorNum = arrayDesordenado[i];
+                
+            }
+        }
+        
+        return maiorNum;
+        
     }
+            
+
 
     public static Integer parMaisProximo(Integer[][] pares){
         return 1;
